9ac091a4ccf14a88bd054648dd414f2a7cd58c5f
aadarshmishra49 <74289413+aadarshmishra49@users.noreply.github.com>
2023-03-03 15:14:08 +0530

    if-else conditional statements

---
+++ conditional.rb
@@ -0,0 +1,29 @@
+# if-else example 
+
+x = 1
+if x > 2
+   puts "x is greater than 2"
+else
+   puts "x is smaller than 2"
+end
+
+#example -2 
+
+y=7
+x="greater" if y>6
+puts x
+x="equal" if y==7
+puts x
+
+
+# nested if-else problems 
+
+
+x = 1
+if x > 2
+   puts "x is greater than 2"
+elsif x <= 2 
+   puts "x is 1"
+else
+   puts "I can't guess the number"
+end
